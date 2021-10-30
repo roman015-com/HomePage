86b13fcc1e3d2ddea8b3584c216c382f096759df
roman015 <xaviervarricatt@live.in>
2021-10-30 21:32:55 +0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,83 @@
+using Blazored.LocalStorage;
+using HomePage.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Radzen;
+using Roman015API.Clients;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomePage
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(sp => new HttpClient { 
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+            });
+
+            Roman015APIClientSetup(builder);
+            RadzenSetup(builder);
+            BlazoredLocalStorageSetup(builder);
+
+            await builder.Build().RunAsync();
+        }
+
+        public static void Roman015APIClientSetup(WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration
+                    .Bind("AzureAd", options.ProviderOptions.Authentication);
+
+                options.ProviderOptions.DefaultAccessTokenScopes
+                    .Add("api://f744ceb4-e6cf-4923-a971-c7fc4a600fe2/ApiScope");
+
+                options.ProviderOptions.LoginMode = "redirect";
+
+            });
+            builder.Services.AddAuthorizationCore(config =>
+            {
+                config.AddPolicy("BlogAdministratorsOnly", policy => policy.AddRequirements(new PermittedRoleRequirement("BlogAdministrator")));
+            });
+
+            builder.Services.AddSingleton<HubConnection>(sp => {
+                return new HubConnectionBuilder()
+                  .WithUrl("https://api.roman015.com/NotificationHub")
+                  .WithAutomaticReconnect()
+                  .Build();
+            });
+
+            #region For FE RBAC
+            builder.Services.AddSingleton<IAuthorizationPolicyProvider, PermittedRolePolicyProvider>();
+            builder.Services.AddSingleton<IAuthorizationHandler, PermittedRoleAuthorizationHandler>();
+            #endregion
+
+            builder.Services.AddScoped<BlogEditorAPIClient>();
+        }
+
+        public static void RadzenSetup(WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<TooltipService>();
+            builder.Services.AddScoped<ContextMenuService>();
+        }
+
+        public static void BlazoredLocalStorageSetup(WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddBlazoredLocalStorage();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# HomePage
+Contains The frontend code for www.roman015.com  
+
+This code is a blazor webassembly project that runs on the client's web browser. This can be run directly from Visual Studio and is hosted in production as a static website using [Azure Static Web App](https://azure.microsoft.com/en-us/services/app-service/static/)
+
+## DEV NOTES
+
+To add GitHub Packages as a NuGet Repository (This is a system wide change):
+
+    dotnet nuget add source --username <USERNAME_HERE> --password <TOKEN_HERE_>  --store-password-in-clear-text --name "Roman015Github" "https://nuget.pkg.github.com/roman015-com/index.json"
+
+Static web app is built using Oryx. Environment varibles for Oryx can be found [here](https://github.com/microsoft/Oryx/blob/main/doc/configuration.md#oryx-configuration). 
