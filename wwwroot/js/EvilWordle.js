f0723e56d6ff802e9a43f287d448aaf141dde7a7
roman015 <xaviervarricatt@live.in>
2022-02-27 18:53:09 +0800

    Input Fix for Mobile

--- wwwroot/js/EvilWordle.js
+++ wwwroot/js/EvilWordle.js
@@ -2,4 +2,8 @@
     $('#EvilWordleModal').modal('show')
 }
 
+export function IsDevice() {
+    console.log(/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent));
+    return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent);
+}
 
